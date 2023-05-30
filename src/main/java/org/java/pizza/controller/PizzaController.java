2c0ff18fe7c47a2b064850d2cef1779c908d9e69
francescotrudu <francescotrudu84@gmail.com>
2023-05-30 17:07:37 +0200

    Added validation

--- src/main/java/org/java/pizza/controller/PizzaController.java
+++ src/main/java/org/java/pizza/controller/PizzaController.java
@@ -8,11 +8,15 @@ import org.java.pizza.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class PizzaController {
@@ -46,11 +50,23 @@ public class PizzaController {
 //	CREATE
 	@GetMapping("/pizze/create")
 	public String createPizza(Model model) {
+		model.addAttribute("pizza", new Pizza());
 		return "create";
 	}
 	
 	@PostMapping("/pizze/create")
-	public String storePizza(@ModelAttribute Pizza pizza) {
+	public String storePizza(Model model,
+							@Valid @ModelAttribute Pizza pizza,
+							BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			for (ObjectError err : bindingResult.getAllErrors())
+				System.err.println("Error: " + err.getDefaultMessage());
+			model.addAttribute("pizza", pizza);
+			model.addAttribute("errors", bindingResult);
+			return "create";
+		}
+		
+		
 		pizzaService.save(pizza);
 		return "redirect:/";
 	}
@@ -78,8 +94,16 @@ public class PizzaController {
 	@PostMapping("/pizze/update/{id}")
 	public String update(
 			@PathVariable Integer id,
-			@ModelAttribute Pizza pizza
-		) {
+			@Valid @ModelAttribute Pizza pizza,
+			BindingResult bindingResult,
+			Model model) {
+		if(bindingResult.hasErrors()) {
+			for (ObjectError err : bindingResult.getAllErrors())
+				System.err.println("Error: " + err.getDefaultMessage());
+			model.addAttribute("pizza", pizza);
+			model.addAttribute("errors", bindingResult);
+			return "update";
+		}
 		pizzaService.save(pizza);
 		return "redirect:/";
 	}
